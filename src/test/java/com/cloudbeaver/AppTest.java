9039deb3d1354c24ebdfd37e2667d6ed229a5c7a
shujie <shangshujie365@gmail.com>
2016-06-22 17:30:55 +0800

    add some small tests

--- src/test/java/com/cloudbeaver/AppTest.java
+++ src/test/java/com/cloudbeaver/AppTest.java
@@ -1,5 +1,9 @@
 package com.cloudbeaver;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import net.sf.json.JSONArray;
 
 import org.junit.AfterClass;
@@ -60,5 +64,21 @@ public class AppTest{
 	public void testJsonArrayEmpty(){
 		JSONArray jArray = new JSONArray();
 		Assert.assertEquals(jArray.toString(),"[]");
+	}
+
+	@Test
+	public void testDate() throws ParseException{
+		String dirConf = "/tmp/testpics?2016-06-22-12-00-00";
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd-hh-mm-ss");
+		String time = dirConf.substring(dirConf.indexOf('?') + 1);
+		Date date = sdf.parse(time);
+		System.out.println(date.getTime() + "," + (new Date()).getTime());
+	}
+
+	@Test
+	public void testString(){
+		String dirConf = "/tmp/testpics?";
+		String sub = dirConf.substring(dirConf.length() - 1);
+		System.out.println(sub);
 	}
 }
